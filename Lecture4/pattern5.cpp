e48a6a3a7c9342ec13e685ac0451ad6857f2e3c7
UDAAKADAL <vaibhavchauhan12353@gmail.com>
2025-05-06 08:20:25 +0530

    Refactor pattern5.cpp to improve code structure and add missing newline at end of file; add compiled executable pattern5.exe

--- Lecture4/pattern5.cpp
+++ Lecture4/pattern5.cpp
@@ -5,11 +5,17 @@ int main()
     cout << "Enter number :";
     int n;
     cin >> n;
-
-   
-
-    
-    
-
-
+    //printing rows
+   for(int row=0;row<n;row++){
+        //printing space
+        for(int space=0;space<n-row-1; space++){
+            cout<<" ";
+        }
+        //printing stars
+        for(int star=0;star<2*row+1;star++){
+            cout<<"*";
+        }
+        cout<<endl;
+   }
+    return 0;
 }
