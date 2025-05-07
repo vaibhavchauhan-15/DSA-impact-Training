924c3500f7021bcf946d9dc5ecbc561c18905c00
vaibhavchauhan-15 <vaibhavchauhan12353@gmail.com>
2025-05-07 13:09:58 +0530

    Add implementations for even and odd number counters in Lecture5

---
+++ Lecture5/even.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[]={12,24,32,2,3,2};
+    int n=6;
+    for(int i=0;i<n;i++){
+        if(arr[i]%2==0){
+
+            cout<<arr[i]<<" ";
+        }
+    }
+}
